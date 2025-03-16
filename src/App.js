5e528c1dbc98fe775a815dc6b124f6dd7867e60e
Senie Calalang <senie.c88@gmail.com>
2025-03-16 15:26:56 +0900

    feat: add VideosSection component and update App.js imports

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-// import Navbar from './components/Navbar'; // Importing Navbar component
+// import Navbar from './components/Navbar';
 // import Footer from './components/Footer';
-import HeroBanner from './components/HeroBanner'; // Importing Hero Banner component
-// import InfoSection from './components/InfoSection'; // Importing Info Section component
-// import VideosSection from './components/VideosSection'; // Importing Videos Section component
-// import NewsSection from './components/NewsSection'; // Importing News Section component
-// import EventsSection from './components/EventsSection'; // Importing Events Section component
+import HeroBanner from './components/HeroBanner';
+import VideosSection from './components/VideosSection';
+// import InfoSection from './components/InfoSection';
+// import NewsSection from './components/NewsSection'; //
+// import EventsSection from './components/EventsSection';
 
 function App() {
   return (
     <Router>
-      {/* Outer container to structure the page layout with flexbox */}
       <div className="flex flex-col min-h-screen overflow-y-auto">
-        {/* Navbar component for navigation links */}
         {/* <Navbar /> */}
-        {/* Hero Banner added before the main content */}
-        {/* <HeroBanner backgroundImage="/path-to-hero-image.jpg" /> */}
         <HeroBanner backgroundImage="/src/images/hisandher-banner.gif" />
 
-        {/* Main content sections stacked vertically */}
-        {/* <main className="flex flex-col space-y-12 p-4">
-          <InfoSection />
+        <main className="flex flex-col space-y-12 p-4">
+          {/* <InfoSection /> */}
           <VideosSection />
-          <NewsSection />
-          <EventsSection />
+          {/* <NewsSection />
+          <EventsSection /> */}
         </main>
-        <Footer /> */}
+        {/* <Footer /> */}
       </div>
     </Router>
   );
