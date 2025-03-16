f1d4c20a62af21359c3d852d95597d1cb2f8d82e
Senie Calalang <senie.c88@gmail.com>
2025-03-16 15:14:44 +0900

    feat: add app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import Navbar from './components/Navbar'; // Importing Navbar component
+// import Footer from './components/Footer';
+import HeroBanner from './components/HeroBanner'; // Importing Hero Banner component
+// import InfoSection from './components/InfoSection'; // Importing Info Section component
+// import VideosSection from './components/VideosSection'; // Importing Videos Section component
+// import NewsSection from './components/NewsSection'; // Importing News Section component
+// import EventsSection from './components/EventsSection'; // Importing Events Section component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* Outer container to structure the page layout with flexbox */}
+      <div className="flex flex-col min-h-screen overflow-y-auto">
+        {/* Navbar component for navigation links */}
+        {/* <Navbar /> */}
+        {/* Hero Banner added before the main content */}
+        {/* <HeroBanner backgroundImage="/path-to-hero-image.jpg" /> */}
+        <HeroBanner backgroundImage="/src/images/hisandher-banner.gif" />
+
+        {/* Main content sections stacked vertically */}
+        {/* <main className="flex flex-col space-y-12 p-4">
+          <InfoSection />
+          <VideosSection />
+          <NewsSection />
+          <EventsSection />
+        </main>
+        <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
